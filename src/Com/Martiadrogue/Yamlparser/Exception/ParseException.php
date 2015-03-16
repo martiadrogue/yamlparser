e577b90bdb83658e5d41d164700d899b6ccf62f7
marti adrogue <marti.adrogue@gmail.com>
2015-03-16 17:26:02 +0100

    Initial commit.

---
+++ src/Com/Martiadrogue/Yamlparser/Exception/ParseException.php
@@ -0,0 +1,15 @@
+<?php
+namespace Com\Martiadrogue\Yamlparser\Exception;
+
+use \RuntimeException;
+
+/**
+ *
+ */
+class ParseException extends RuntimeException
+{
+    public function __construct()
+    {
+        # code...
+    }
+}
